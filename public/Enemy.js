54c2afbe31575a321efcc1cde36f62c4f5430993
GiliEllen <gilimena95@gmail.com>
2022-04-10 20:23:42 +0300

    enemies

--- public/Enemy.js
+++ public/Enemy.js
@@ -3,7 +3,8 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Enemy_instances, _Enemy_loadImages;
+var _Enemy_instances, _Enemy_loadImages, _Enemy_random, _Enemy_move, _Enemy_changeDirection;
+import { MovingDirection } from "./MoveDirection.js";
 export class Enemy {
     constructor(x, y, tileSize, velocity, tileMap) {
         _Enemy_instances.add(this);
@@ -12,12 +13,20 @@ export class Enemy {
         this.tileSize = tileSize;
         this.velocity = velocity;
         this.tileMap = tileMap;
-        this.normalGhost = null;
-        this.scaredGhost = null;
-        this.scaredGhost2 = null;
+        this.normalGhost = new Image();
+        this.scaredGhost = new Image();
+        this.scaredGhost2 = new Image();
+        this.image = this.normalGhost;
+        this.movingDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
+        this.directionTimerDefault = __classPrivateFieldGet(this, _Enemy_instances, "m", _Enemy_random).call(this, 10, 50);
+        this.directionTimer = this.directionTimerDefault;
         __classPrivateFieldGet(this, _Enemy_instances, "m", _Enemy_loadImages).call(this);
     }
-    draw() { }
+    draw(ctx) {
+        __classPrivateFieldGet(this, _Enemy_instances, "m", _Enemy_move).call(this);
+        __classPrivateFieldGet(this, _Enemy_instances, "m", _Enemy_changeDirection).call(this);
+        ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+    }
 }
 _Enemy_instances = new WeakSet(), _Enemy_loadImages = function _Enemy_loadImages() {
     this.normalGhost = new Image();
@@ -26,4 +35,38 @@ _Enemy_instances = new WeakSet(), _Enemy_loadImages = function _Enemy_loadImages
     this.scaredGhost.src = '../images/scaredGhost.png';
     this.scaredGhost2 = new Image();
     this.scaredGhost2.src = '../images/scaredGhost2.png';
+    this.image = this.normalGhost;
+}, _Enemy_random = function _Enemy_random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}, _Enemy_move = function _Enemy_move() {
+    if (!this.tileMap.didCollideWithEnviorment(this.x, this.y, this.movingDirection)) {
+        switch (this.movingDirection) {
+            case MovingDirection.up:
+                this.y -= this.velocity;
+                break;
+            case MovingDirection.down:
+                this.y += this.velocity;
+                break;
+            case MovingDirection.left:
+                this.x -= this.velocity;
+                break;
+            case MovingDirection.right:
+                this.x += this.velocity;
+                break;
+        }
+    }
+}, _Enemy_changeDirection = function _Enemy_changeDirection() {
+    this.directionTimer--;
+    let newMoveDirection = null;
+    if (this.directionTimer === 0) {
+        this.directionTimer = this.directionTimerDefault;
+        newMoveDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
+    }
+    if (newMoveDirection !== null && this.movingDirection !== newMoveDirection) {
+        if (Number.isInteger(this.x / this.tileSize) && Number.isInteger(this.y / this.tileSize)) {
+            if (!this.tileMap.didCollideWithEnviorment(this.x, this.y, newMoveDirection)) {
+                this.movingDirection = newMoveDirection;
+            }
+        }
+    }
 };
--- src/Enemy.ts
+++ src/Enemy.ts
@@ -1,3 +1,4 @@
+import { MovingDirection } from "./MoveDirection.js";
 import { TileMap } from "./TileMap.js";
 
 export class Enemy {
@@ -5,10 +6,14 @@ export class Enemy {
     y: number;
     tileSize: number;
     velocity: number;
-    normalGhost: HTMLImageElement | null;
-    scaredGhost: HTMLImageElement | null;
-    scaredGhost2: HTMLImageElement | null;
+    normalGhost: HTMLImageElement;
+    scaredGhost: HTMLImageElement;
+    scaredGhost2: HTMLImageElement;
     tileMap: TileMap;
+    image: HTMLImageElement;
+    movingDirection: number;
+    directionTimerDefault: number;
+    directionTimer: number;
 
     constructor(x: number, y: number, tileSize: number, velocity: number, tileMap: TileMap) {
         this.x = x;
@@ -17,14 +22,23 @@ export class Enemy {
         this.velocity = velocity;
         this.tileMap = tileMap;
 
-        this.normalGhost = null;
-        this.scaredGhost = null;
-        this.scaredGhost2 = null;
+        this.normalGhost = new Image();
+        this.scaredGhost = new Image();
+        this.scaredGhost2 = new Image();
+        this.image = this.normalGhost;
+
+        this.movingDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
+        this.directionTimerDefault = this.#random(10, 50);
+        this.directionTimer = this.directionTimerDefault;
 
         this.#loadImages();
     }
 
-    draw() { }
+    draw(ctx: CanvasRenderingContext2D) {
+        this.#move();
+        this.#changeDirection();
+        ctx.drawImage(this.image, this.x, this.y, this.tileSize, this.tileSize);
+    }
 
     #loadImages() {
         this.normalGhost = new Image();
@@ -35,5 +49,46 @@ export class Enemy {
 
         this.scaredGhost2 = new Image();
         this.scaredGhost2.src = '../images/scaredGhost2.png';
+
+        this.image = this.normalGhost;
+    }
+
+    #random(min: number, max: number) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    #move() {
+        if (!this.tileMap.didCollideWithEnviorment(this.x, this.y, this.movingDirection)) {
+            switch (this.movingDirection) {
+                case MovingDirection.up:
+                    this.y -= this.velocity;
+                    break;
+                case MovingDirection.down:
+                    this.y += this.velocity;
+                    break;
+                case MovingDirection.left:
+                    this.x -= this.velocity;
+                    break;
+                case MovingDirection.right:
+                    this.x += this.velocity;
+                    break;
+            }
+        }
+    }
+
+    #changeDirection() {
+        this.directionTimer--;
+        let newMoveDirection = null;
+        if (this.directionTimer === 0) {
+            this.directionTimer = this.directionTimerDefault;
+            newMoveDirection = Math.floor(Math.random() * Object.keys(MovingDirection).length);
+        }
+        if (newMoveDirection !== null && this.movingDirection !== newMoveDirection) {
+            if (Number.isInteger(this.x / this.tileSize) && Number.isInteger(this.y / this.tileSize)) {
+                if(!this.tileMap.didCollideWithEnviorment(this.x, this.y, newMoveDirection)) {
+                    this.movingDirection = newMoveDirection;
+                }
+            }
+        }
     }
 }
